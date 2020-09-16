feba8f97c9c60a6d69aa0f2891673922185ba547
Danny Tuppeny <danny@tuppeny.com>
2020-09-16 16:55:36 +0100

    REPRO

--- src/extension/providers/dart_diagnostic_provider.ts
+++ src/extension/providers/dart_diagnostic_provider.ts
@@ -2,38 +2,19 @@ import { Diagnostic, DiagnosticCollection, DiagnosticSeverity, Uri } from "vscod
 import * as as from "../../shared/analysis_server_types";
 import { toRangeOnLine } from "../../shared/vscode/utils";
 import { DasAnalyzerClient } from "../analysis/analyzer_das";
-import { config } from "../config";
 
 // TODO: This is not a provider?
 export class DartDiagnosticProvider {
-	private lastErrorJson: string | undefined;
 	constructor(private readonly analyzer: DasAnalyzerClient, private readonly diagnostics: DiagnosticCollection) {
 		this.analyzer.registerForAnalysisErrors((es) => this.handleErrors(es));
 	}
 
 	private handleErrors(notification: as.AnalysisErrorsNotification) {
-		const notificationJson = JSON.stringify(notification);
-
-		// As a workaround for https://github.com/Dart-Code/Dart-Code/issues/1678, if
-		// the errors we got are exactly the same as the previous set, do not give
-		// them to VS Code. This avoids a potential loop of refreshing the error view
-		// which triggers a request for Code Actions, which could result in analysis
-		// of the file (which triggers errors to be sent, which triggers a refresh
-		// of the error view... etc.!).
-		if (this.lastErrorJson === notificationJson) {
-			// TODO: Come up with a better fix than this!
-			// log("Skipping error notification as it was the same as the previous one");
-			return;
-		}
-
-		let errors = notification.errors;
-		if (!config.showTodos)
-			errors = errors.filter((error) => error.type !== "TODO");
+		const errors = notification.errors;
 		this.diagnostics.set(
 			Uri.file(notification.file),
 			errors.map((e) => DartDiagnosticProvider.createDiagnostic(e)),
 		);
-		this.lastErrorJson = notificationJson;
 	}
 
 	public static createDiagnostic(error: as.AnalysisError): Diagnostic {
